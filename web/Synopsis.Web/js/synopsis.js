f1b575a7faab29fd63869f8df4453fc270301f2c
mphrase <mfras3r@hotmail.com>
2015-07-23 15:59:41 -0400

    Updating home page to use the web service, and render results.

---
+++ web/Synopsis.Web/js/synopsis.js
@@ -0,0 +1,47 @@
+﻿angular.module("synopsis", ["ui.bootstrap"])
+	.controller("SynopsisController", [
+		"$scope", "$http", "$log", '$timeout',
+		function ($scope, $http, $log, $timeout) {
+			$scope.url = "";
+			$scope.alerts = [];
+
+			$scope.request = function (isValid) {
+				if (!isValid) {
+					return;
+				}
+
+				// Reset results scope.
+				$scope.words = [];
+				$scope.images = [];
+				$scope.loading = true;
+				$scope.showResults = false;
+
+				$http.get("/request", {
+						params: {
+							url: $scope.url
+						}
+					})
+					.success(function(data, status, headers, config) {
+						$log.debug("Received: ", data);
+						$scope.images = data.images;
+						$log.debug("Setting images: ", $scope.images);
+
+						$log.debug("Setting words: ", $scope.words);
+						$scope.words = data.words;
+
+						$scope.loading = false;
+						$scope.showResults = true;
+					})
+					.error(function(data, status, headers, config) {
+						$log.warn(data);
+						$scope.error = true;
+						$scope.errorMessage = data.message;
+						$scope.loading = false;
+
+						$timeout(function () {
+							$scope.error = false;
+						}, 10000);
+					});
+			};
+		}
+	]);
